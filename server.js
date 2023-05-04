178e33a1ee17382829eb4102cd81d96998db0f0a
mileRANT <liem.htran@outlook.com>
2023-05-04 16:52:13 -0400

    Handling Response on express.js

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express();  //looks similar to python/flask
 
 // app.listen(3000); //listens to a specific port. run this by using node
 //note that ctrl+c will exit the node loop
+//often request and response and shortened to req and res
+app.get("/", function(req, res){
+    // console.log(request);
+    res.send("Hello");
+});
+
 
 app.listen(3000, function(){
     //callback function so we can see it respond in node
