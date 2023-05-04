219898161579a6c6282cc78ca6b3562f6b8d51c9
mileRANT <liem.htran@outlook.com>
2023-05-04 16:26:34 -0400

    Running express.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 const express = require("express");
+
+const app = express();  //looks similar to python/flask
+
+// app.listen(3000); //listens to a specific port. run this by using node
+//note that ctrl+c will exit the node loop
+
+app.listen(3000, function(){
+    //callback function so we can see it respond in node
+    console.log("Server started on port 3000");
+});
